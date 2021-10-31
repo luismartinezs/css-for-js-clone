55d020c654ea007116a2bfb6ea13fc09bdf17f75
lmartinezs <martinez.s.luis@gmail.com>
2021-10-31 13:46:29 +0100

    Added windicss

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,12 +1,12 @@
 <template>
-  <header>
+  <header class="h-12 fixed z-10">
     <nav>
       <ul>
         <li>
           <a href="#">Login</a>
         </li>
         <li>
-          <button>Enroll</button>
+          <button class="px-6 py-4 bg-pink-500 rounded">Enroll</button>
         </li>
       </ul>
     </nav>
